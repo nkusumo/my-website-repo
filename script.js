bfdc5f6f6d10f19e3497950ed30a1db7004ddc26
Nisa Kusumo <nis.kusumo@gmail.com>
2021-05-10 00:31:55 -0700

    added images to html, formatted for overlapping image and text, added javascript for clicking on images

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-// const h2 = document.createElement('h2');
-// h2.textContent = "This content added by JavaScript"; 
-// document.querySelector('body').appendChild(h2);
+const images = document.getElementsByTagName('img');
+
+function changeOpacity(image) {
+    let opacityPct = parseInt(image.style.opacity);
+    if (opacityPct > 0.1) {
+        image.style.opacity = "0.1";
+    }
+    else {
+        image.style.opacity = "1";
+    }
+}
+
+for (let i=0; i < images.length; i++) {
+    images[i].addEventListener('click', function() { changeOpacity(images[i]) });
+}
